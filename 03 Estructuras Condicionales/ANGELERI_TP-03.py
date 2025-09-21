211876fd2d29b1d46c08ad24cd67582746d4641b
Luciano Angeleri <lcnang@gmail.com>
2025-09-21 01:07:17 -0300

    Se resolvió el ejercicio 6

--- 03 Estructuras Condicionales/ANGELERI_TP-03.py
+++ 03 Estructuras Condicionales/ANGELERI_TP-03.py
@@ -50,10 +50,52 @@
 
 #5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres (incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal como una lista o un string.
 
-password = input("Ingrese su contraseña, debe contener entre 8 y 14 caracteres:")
+# password = input("Ingrese su contraseña, debe contener entre 8 y 14 caracteres:")
 
-if len(password) >= 8 and len(password) <= 14:
-    print("Ha ingresado una contraseña correcta")
+# if len(password) >= 8 and len(password) <= 14:
+#     print("Ha ingresado una contraseña correcta")
+# else:
+#     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+
+#-----------------------------------------------------------------------------------------------------------------
+
+#6) El paquete statistics de python contiene funciones que permiten tomar una lista de números y calcular la moda, la mediana y la media de dichos números. Un ejemplo de su uso es el siguiente:
+#from statistics import mode, median, mean
+# mi_lista = [1,2,5,5,3]
+#mean(mi_lista)
+# En la documentación oficial se puede encontrar más información sobre este paquete:
+# https://docs.python.org/es/3.8/library/statistics.html.
+# La moda (mode), la mediana (median) y la media (mean) son parámetros estadísticos que se
+# pueden utilizar para predecir la forma de una distribución normal a partir del siguiente criterio:
+# ● Sesgo positivo o a la derecha: cuando la media es mayor que la mediana y, a su vez, la mediana es mayor que la moda.
+# ● Sesgo negativo o a la izquierda: cuando la media es menor que la mediana y, a su vez, la mediana es menor que la moda.
+# ● Sin sesgo: cuando la media, la mediana y la moda son iguales.
+# Teniendo en cuenta lo antes mencionado, escribir un programa que tome la lista numeros_aleatorios, calcule su moda, su mediana y su media y las compare para determinar si hay sesgo positivo, negativo o no hay sesgo. Imprimir el resultado por pantalla.
+# Definir la lista numeros_aleatorios de la siguiente forma:
+# import random
+# numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
+# Nota: el bloque de código anterior crea una lista con 50 números entre 1 y 100 elegidos de forma aleatoria.
+
+from statistics import mode, median, mean
+import random
+
+numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
+
+mediana= median(numeros_aleatorios)
+moda= mode(numeros_aleatorios)
+media= mean(numeros_aleatorios)
+
+print(f"Números:{numeros_aleatorios}")
+print("----------------------------------")
+print(f"Mediana:{mediana}")
+print(f"Moda:{moda}")
+print(f"Media:{media}")
+
+if (media > mediana) and (mediana > moda):
+    print("Hay sesgo positivo o a la derecha")
+elif (media < mediana) and (mediana < moda):
+    print("Hay sesgo negativo o a la izquierda")
+elif media == mediana == moda:
+    print("Sin sesgo")
 else:
-    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
-
+    print("No se encontró un tipo de sesgo identificable")
