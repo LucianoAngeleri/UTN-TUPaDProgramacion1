94fc0edab34ec9d664351cd8f688fc9f981a9eca
Luciano Angeleri <lcnang@gmail.com>
2025-11-17 23:31:34 -0300

    Se resolvió el ejercicio 2 del TP6

--- 06 Funciones/ANGELERI_TP-06.py
+++ 06 Funciones/ANGELERI_TP-06.py
@@ -1,16 +1,20 @@
 #1) Crear una función llamada imprimir_hola_mundo que imprima por pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el programa principal.
 
-print("Ejercicio 1")
+# print("Ejercicio 1")
 
-def imprimir_hola_mundo():
-    print("Hola Mundo!")
-imprimir_hola_mundo()
+# def imprimir_hola_mundo():
+#     print("Hola Mundo!")
+# imprimir_hola_mundo()
 
 
 #2) Crear una función llamada saludar_usuario(nombre) que reciba como parámetro un nombre y devuelva un saludo personalizado. Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa principal solicitando el nombre al usuario.
 
-#print("Ejercicio 2")
+print("Ejercicio 2")
 
+def saludar_usuario(nombre):
+    print(f"Hola {nombre}! Cómo estas?")
+nombre_usuario = input("Ingrese su nombre: ")
+saludar_usuario(nombre_usuario)
 
 #3) Crear una función llamada informacion_personal(nombre, apellido, edad, residencia) que reciba cuatro parámetros e imprima: “Soy [nombre] [apellido], tengo [edad] años y vivo en [residencia]”. Pedir los datos al usuario y llamar a esta función con los valores ingresados.
 
