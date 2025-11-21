65278ca29e504ff9be921e8b62070eaa75276dd7
César Luciano Angeleri <lcnang@gmail.com>
2025-11-21 14:59:51 -0300

    Ss añadió el enunciado y la resolución del ejericio 2 del TP9 de Recursividad

--- 09 Recursividad/ANGELERI_TP-09.py
+++ 09 Recursividad/ANGELERI_TP-09.py
@@ -1,13 +1,31 @@
 # 1) Crea una función recursiva que calcule el factorial de un número. Luego, utiliza esa función para calcular y mostrar en pantalla el factorial de todos los números enteros entre 1 y el número que indique el usuario.
 
-print("Ejercicio 1")
+# print("Ejercicio 1")
 
-def factorial_recursivo(num):
+# def factorial_recursivo(num):
+#     if num == 0:
+#         return 1
+#     else:
+#         return num * factorial_recursivo(num-1)
+    
+# numero_ingresado = int(input("Ingrese el número para calcular su factorial: "))
+# for i in range(1, numero_ingresado + 1):
+#     print(f"El factorial de {i} es: {factorial_recursivo(i)}")
+
+# 2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario especifique.
+
+print("Ejercicio 2")
+
+def fibonacci(num):
     if num == 0:
+        return 0
+    elif num == 1:
         return 1
     else:
-        return num * factorial_recursivo(num-1)
-    
-numero_ingresado = int(input("Ingrese el número para calcular su factorial: "))
-for i in range(1, numero_ingresado + 1):
-    print(f"El factorial de {i} es: {factorial_recursivo(i)}")
+        return fibonacci(num-1) + fibonacci(num-2)
+
+numero_ingresado = int(input("Ingrese el número para calcular ver la serie de Fibonacci hasta esta posición: "))
+print("Serie de Fibonacci:")
+for i in range(numero_ingresado + 1):
+    print(f"Posición:{i} = {fibonacci(i)}")
+
